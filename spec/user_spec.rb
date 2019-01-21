9d355ed00bb7eab05e81fadecb94fc13bae5e85e
marie goudon <marie.goudon@laposte.net>
2019-01-21 20:57:29 +0100

    ajout de tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/user'
 
-describe User do 
+describe "the User class" do 
+	it "contains users" do
 
 	before(:each) do
 	
@@ -8,12 +9,28 @@ describe User do
 		Object.send(:remove_const, 'User')
 		load 'user.rb'
 	end
-	describe 'initializer' do 
+	describe 'the initializer method' do 
 	
 		it 'creates an user' do
-			user = User.new("email@email.com")
+			#user = User.new("email@email.com")
 			expect(user.class).to eq(User)
 		end
 	end
+	describe "the update_email method" do
+		it 'updates the email' do
+			expect(update_email(email_to_update).to eq(email))
+			expect(read_email).to eq(email)
+			expect(User.find_by_user("nom@site.com")).to eq("user")
+			
+		end
+	end
+describe "the Event class" do 
+	it "contains the upcoming events" do 
+		
+	describe "the initialize method of the Event class" do
+		it "gives the basic info of the event" do 
+
+
+				
 end
 
